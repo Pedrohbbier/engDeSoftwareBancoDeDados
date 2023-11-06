0e06db1bc68bc47e604db0f90272537a8e8a0a48
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2023-11-06 16:39:37 -0300

    Terminando exercício extra

--- Atividades MySQL - Aluno REV-1(1) (1).sql
+++ Atividades MySQL - Aluno REV-1(1) (1).sql
@@ -195,59 +195,152 @@ select nome_material, quantidade_estoque from quantidade_de_materiais_por_nome_d
 
 /* 10 - Nome do material, sua quantidade em estoque e sua quantidade mínima em estoque */
 
+select nome_material, quantidade_estoque, quantidade_estoque_minima from materiais;
+
 /* 11 - Nome do material, sua quantidade em estoque e sua quantidade mínima em estoque,
 apenas para materiais onde a quantidade em estoque esteja abaixo da quantidade mínima em estoque */
 
+select nome_material, quantidade_estoque, quantidade_estoque_minima
+from materiais
+where quantidade_estoque < quantidade_estoque_minima;
+
 /* 12 - Nome e cidade dos fornecedores */
+
+select nome_fornecedor, cidade
+from fornecedores;
 
 /* 13 - Nome e cidade dos fornecedores da cidade de Curitiba */
 
+select nome_fornecedor, cidade
+from fornecedores
+where cidade = 'Curitiba';
+
+
 /* 14 - Nome dos fornecedores e seus materiais */
+
+select f.nome_fornecedor, m.nome_material
+from fornecedores f
+join materiais m on f.id_fornecedor = m.id_fornecedor;
 
 /* 15 - Nome dos fornecedores e seus materiais, inluindo os fornecedores sem nenhum material reacionado */
 
+select f.nome_fornecedor, m.nome_material
+from fornecedores f
+left join materiais m on f.id_fornecedor = m.id_fornecedor;
+
 /* 16 - Nome dos fornecedores e quantidade de materiais fornecidos pelo mesmo */
+
+select f.nome_fornecedor, count(m.id_material) as quantidade_de_materiais
+from fornecedores f
+join materiais m on f.id_fornecedor = m.id_fornecedor
+group by f.nome_fornecedor;
 
 /* 17 - Nome dos fornecedores e quantidade de materiais fornecidos pelo mesmo,
 incluindo os fornecedores sem nenhum material relacionado */
+
+select f.nome_fornecedor, count(m.id_material) as quantidade_de_materiais
+from fornecedores f
+left join materiais m on f.id_fornecedor = m.id_fornecedor
+group by f.nome_fornecedor;
 
 /* 18 - Nome dos fornecedores e quantidade de materiais fornecidos pelo mesmo,
 incluindo os fornecedores sem nenhum material relacionado,
 apenas para fornecedores com menos de 5 materiais relacionados */
 
+select f.nome_fornecedor, count(m.id_material) as quantidade_de_materiais
+from fornecedores f
+left join materiais m on f.id_fornecedor = m.id_fornecedor
+group by f.nome_fornecedor
+having count(m.id_material) < 5;
+
 /* 19 - Nome dos fornecedores, nome do material e sua quantidade em estoque */
+
+select f.nome_fornecedor, m.nome_material, m.quantidade_estoque
+from fornecedores f
+join materiais m on f.id_fornecedor = m.id_fornecedor;
+
 
 /* 20 - Nome dos fornecedores, nome do material e sua quantidade em estoque,
 apenas para quatidade em estoque entre 10 e 20 */
 
+select f.nome_fornecedor, m.nome_material, m.quantidade_estoque
+from fornecedores f
+join materiais m on f.id_fornecedor = m.id_fornecedor
+where m.quantidade_estoque between 10 and 20;
+
 /* 21 - Nome do fornecedor, nome do material e nome da unidade,
 apenas para fornecedores de materiais em unidades ou metros */
 
+select f.nome_fornecedor, m.nome_material, u.nome_unidade
+from fornecedores f
+join materiais m on f.id_fornecedor = m.id_fornecedor
+join unidades u on m.id_unidade = u.id_unidade
+where u.nome_unidade in ('Unidades', 'Metros');
+
 /* 22 - Pedidos realizados em 2015 */
+
+select *
+from pedidos
+where year(data_pedido) = 2015;SELECT *
+from pedidos
+where year(data_pedido) = 2015;
+
 
 /* 23 - Pedidos realizados em fevereiro de 2015 */
 
+select *
+from pedidos
+where year(data_pedido) = 2015 and month(data_pedido) = 2;
+
 /* 24 - Número do pedido e o nome dos materiais constantes no pedido */
+
+select ip.id_pedido, m.nome_material
+from itens_pedido ip
+join materiais m on ip.id_material = m.id_material;
 
 /* 25 - Materiais que constam nos pedidos */
 
+select distinct m.nome_material
+from materiais m
+join itens_pedido ip on m.id_material = ip.id_material;
+
 /* 26 - Materiais que não constam nos pedidos */
+
+select id_pedido, count(id_item_pedido) as quantidade_de_itens
+from itens_pedido
+group by id_pedido;
 
 /* 27 - Número do pedido e a quantidade de itens em cada pedido */
 
+select id_pedido, count(id_item_pedido) as quantidade_de_itens
+from itens_pedido
+group by id_pedido;
+
+
 /* 28 - Número do pedido e o valor total do pedido */
+
+select p.id_pedido, sum(ip.quantidade_pedida * ip.valor_unitario) AS valor_total_pedido
+from pedidos p
+join itens_pedido ip on p.id_pedido = ip.id_pedido
+group by p.id_pedido;
 
 /* 29 - Número do pedido e o valor total do pedido, apenas para pedidos com valor total abaixo de 100,00 */
 
+select id_pedido, sum(quantidade_pedida * valor_unitario) as valor_total_pedido
+from itens_pedido
+group by id_pedido
+having valor_total_pedido < 100.00;
+
 /* 30 - Insira mais um fornecedor no banco de dados */
 
+insert into fornecedores (id_fornecedor, nome_fornecedor, cidade, uf)
 values
 ('5', 'Distribuidora de Materiais Curitiba', 'Curitiba', 'PR');
 
 select * from fornecedores;
 
 /* 31 - Insira mais 2 materiais para o fornecedor inserido no item 30 */
-
+insert into materiais (id_material, id_fornecedor, nome_material, quantidade_estoque, quantidade_estoque_minima, id_unidade)
 values
 ('14', '5', 'Borracha', '100', '10', '1'),
 ('15', '5', 'Apontador', '100', '10', '1');
@@ -256,15 +349,15 @@ select * from materiais;
 
 /* 32 - Insira mais um pedido no banco de dados */
 
-
+insert into pedidos (id_pedido, data_pedido)
 values
-('4', '2021-10-21');
+('4', '2021-10-21'); 
 
 select * from pedidos;
 
 /* 33 - Insira 2 itens de pedido no banco de dados, utilizando os dados inseridos nos itens 30, 31 e 32 */ 
 
-
+insert into itens_pedido (id_item_pedido, id_pedido, id_material, quantidade_pedida, valor_unitario)
 values
 ('5', '4', '14', '100', '0.40'),
 ('6', '4', '15', '100', '0.20');
@@ -273,25 +366,37 @@ select * from itens_pedido;
 
 /* 34 - Número do pedido e o valor total do pedido inserido no item 32 */
 
+select p.id_pedido, sum(ip.quantidade_pedida * ip.valor_unitario) as valor_total_pedido
+from pedidos p
+join itens_pedido ip on p.id_pedido = ip.id_pedido
+where p.id_pedido = 4
+group by p.id_pedido;
+
 /* 35 - Exclua o material "Tomada elétrica 10A padrão novo" do banco de dados */
 
+delete from materiais
+where nome_material = 'Tomada elétrica 10A padrão novo';
 
 select * from materiais;
 
 /* 36 - Exclua o material "Lápis Preto HB" do banco de dados */
+delete from itens_pedido
+where id_material = (select id_material from materiais where nome_material = 'Lápis Preto HB');
 
+delete from materiais
+where nome_material = 'Lápis Preto HB';
 
 select * from itens_pedido;
-
-
 select * from materiais;
 
 /* 37 - Exclua o fornecedor "HidraX Materiais Elétricos e Hidraulicos" do banco de dados */
 
+delete from fornecedores
+where nome_fornecedor = 'HidraX Materiais Elétricos e Hidraulicos';
 
 select * from fornecedores;
 
 /* 38 - Exclua o fornecedor "XYZ Materiais de Escritório" do banco de dados */
 
-select * from materiais;
-
+delete from fornecedores
+where nome_fornecedor = 'XYZ Materiais de Escritório';
